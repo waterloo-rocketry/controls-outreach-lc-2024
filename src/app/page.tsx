23ece2ad0b0de484d6dd48a4f7bc492a88fdd8da
Patrick Gu <patrick@patrickgu.ca>
2024-08-03 18:36:35 -0400

    trajectory prediction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import bannerLogo from "@/images/colour_horizontal_standard.png";
 import { useEffect, useReducer, useRef, useState } from "react";
 import {
   defaultRocketState,
+  predictTrajectory,
   RocketState,
   step,
 } from "@/calculations/calculations";
@@ -18,14 +19,15 @@ type GameStatus = (
   | {
       status: "finished";
     }
-) & { state: RocketState };
+) & { trajectory: RocketState[] };
 
 export default function Home() {
   const [ext, setExt] = useState(58);
   const extRef = useRef(58);
+  const [extTraj, setExtTraj] = useState(58);
   const [status, setStatus] = useState<GameStatus>({
     status: "paused",
-    state: defaultRocketState,
+    trajectory: [defaultRocketState],
   });
   const [timeWarp, setTimeWarp] = useState(1);
   useEffect(() => {
@@ -34,14 +36,18 @@ export default function Home() {
         if (status.status === "running") {
           const now = performance.now();
           const state = step(
-            status.state,
+            status.trajectory[status.trajectory.length - 1],
             ((performance.now() - status.lastStepMs) * timeWarp) / 1000,
             extRef.current / 100
           );
           if (state === null) {
-            return { status: "finished", state: status.state };
+            return { status: "finished", trajectory: status.trajectory };
           }
-          return { status: "running", lastStepMs: now, state };
+          return {
+            status: "running",
+            lastStepMs: now,
+            trajectory: [...status.trajectory, state],
+          };
         } else {
           return status;
         }
@@ -51,116 +57,147 @@ export default function Home() {
   }, [timeWarp]);
   const [target, setTarget] = useState(6660);
 
+  const currentState = status.trajectory[status.trajectory.length - 1];
+
+  const predicted = predictTrajectory(currentState, extTraj / 100);
+
   return (
-    <main className="flex min-h-screen w-full">
-      <div className="flex flex-col gap-4 p-4 border-r shadow w-96">
-        <div className="flex justify-center">
-          <div className="w-64 h-16 relative">
-            <Image src={bannerLogo} alt="Waterloo Rocketry Logo" fill />
+    <main className="flex h-screen w-full">
+      <div className="overflow-y-scroll border-r shadow">
+        <div className="flex flex-col gap-4 p-4 w-96 min-h-screen">
+          <div className="flex justify-center">
+            <div className="w-64 h-16 relative">
+              <Image src={bannerLogo} alt="Waterloo Rocketry Logo" fill />
+            </div>
           </div>
-        </div>
-        <h1 className="text-3xl text-center font-bold">Controls Game</h1>
-        <p>Use the slider to control the airbrakes and reach the target!</p>
-        <div className="flex gap-2">
-          <button
-            onClick={() => {
-              if (status.status === "paused") {
-                setStatus({
-                  status: "running",
-                  lastStepMs: performance.now(),
-                  state: status.state,
-                });
-              } else if (status.status === "running") {
+          <h1 className="text-3xl text-center font-bold">Controls Game</h1>
+          <p>Use the slider to control the airbrakes and reach the target!</p>
+          <div className="flex gap-2">
+            <button
+              onClick={() => {
+                if (status.status === "paused") {
+                  setStatus({
+                    status: "running",
+                    lastStepMs: performance.now(),
+                    trajectory: status.trajectory,
+                  });
+                } else if (status.status === "running") {
+                  setStatus({
+                    status: "paused",
+                    trajectory: status.trajectory,
+                  });
+                }
+              }}
+              className={`px-4 py-2 rounded transition flex-1 ${status.status === "running" ? "bg-red-300" : "bg-yellow-300"} disabled:bg-gray-300`}
+              disabled={status.status === "finished"}
+            >
+              {status.status === "running" ? "Pause" : "Play"}
+            </button>
+            <button
+              onClick={() => {
                 setStatus({
                   status: "paused",
-                  state: status.state,
+                  trajectory: [defaultRocketState],
                 });
-              }
+              }}
+              className="px-4 py-2 rounded transition bg-purple-300 flex-1"
+            >
+              Restart
+            </button>
+          </div>
+
+          <hr />
+
+          <label htmlFor="extension" className="text-center">
+            Extension: {ext}%
+          </label>
+          <input
+            type="range"
+            name="extension"
+            min={0}
+            max={100}
+            step={1}
+            value={ext}
+            onChange={(e) => {
+              setExt(e.target.valueAsNumber);
+              extRef.current = e.target.valueAsNumber;
             }}
-            className={`px-4 py-2 rounded transition flex-1 ${status.status === "running" ? "bg-red-300" : "bg-yellow-300"} disabled:bg-gray-300`}
-            disabled={status.status === "finished"}
-          >
-            {status.status === "running" ? "Pause" : "Play"}
-          </button>
-          <button
-            onClick={() => {
-              setStatus({
-                status: "paused",
-                state: defaultRocketState,
-              });
+          />
+          <label htmlFor="extension-trajectory" className="text-center">
+            Extension for trajectory prediction: {extTraj}%
+          </label>
+          <input
+            type="range"
+            name="extension-trajectory"
+            min={0}
+            max={100}
+            step={1}
+            value={extTraj}
+            onChange={(e) => {
+              setExtTraj(e.target.valueAsNumber);
             }}
-            className="px-4 py-2 rounded transition bg-purple-300 flex-1"
-          >
-            Restart
-          </button>
+          />
+
+          <hr />
+
+          <p>Time: {currentState.t_s.toFixed(2)} s</p>
+          <p>Altitude: {currentState.y_m.toFixed(0)} m</p>
+          <p>
+            Vertical Velocity: {Math.max(currentState.vy_m_s, 0.0).toFixed(0)}{" "}
+            m/s
+          </p>
+          <p>Lateral Distance: {currentState.x_m.toFixed(0)} m</p>
+          <p>Lateral Velocity: {currentState.vx_m_s.toFixed(0)} m/s</p>
+
+          <hr />
+
+          <div className="flex gap-4">
+            <label htmlFor="target" className="w-32">
+              Target: {target} m
+            </label>
+            <input
+              type="range"
+              name="target"
+              min={6410}
+              max={6810}
+              step={10}
+              value={target}
+              onChange={(e) => {
+                setTarget(e.target.valueAsNumber);
+              }}
+              className="flex-grow"
+            />
+          </div>
+
+          <div className="flex gap-4">
+            <label htmlFor="time-warp" className="w-32">
+              Time warp: x{timeWarp.toFixed(1)}
+            </label>
+            <input
+              type="range"
+              name="time-warp"
+              min={0}
+              max={5}
+              step={0.1}
+              value={timeWarp}
+              onChange={(e) => {
+                setTimeWarp(e.target.valueAsNumber);
+              }}
+              className="flex-grow"
+            />
+          </div>
+
+          <hr />
+
+          <p>A video game to see what it&apos;s like to control a rocket!</p>
+          <p>
+            Based off real aerodynamic data from Cycle 4 of our Borealis rocket.
+          </p>
+          <p>Controls Outreach Game. Made with ❤️ by Waterloo Rocketry.</p>
         </div>
-
-        <hr />
-
-        <label htmlFor="extension" className="text-center">
-          Extension: {ext}%
-        </label>
-        <input
-          type="range"
-          name="extension"
-          min={0}
-          max={100}
-          step={1}
-          value={ext}
-          onChange={(e) => {
-            setExt(e.target.valueAsNumber);
-            extRef.current = e.target.valueAsNumber;
-          }}
-        />
-
-        <hr />
-
-        <p>Time: {status.state.t_s.toFixed(2)} s</p>
-        <p>Altitude: {status.state.y_m.toFixed(0)} m</p>
-        <p>
-          Vertical Velocity: {Math.max(status.state.vy_m_s, 0.0).toFixed(0)} m/s
-        </p>
-
-        <hr />
-
-        <label htmlFor="target" className="text-center">
-          Target: {target} m
-        </label>
-        <input
-          type="range"
-          name="target"
-          min={6410}
-          max={6810}
-          step={10}
-          value={target}
-          onChange={(e) => {
-            setTarget(e.target.valueAsNumber);
-          }}
-        />
-
-        <label htmlFor="time-warp" className="text-center">
-          Time warp: x{timeWarp.toFixed(1)}
-        </label>
-        <input
-          type="range"
-          name="time-warp"
-          min={0}
-          max={5}
-          step={0.1}
-          value={timeWarp}
-          onChange={(e) => {
-            setTimeWarp(e.target.valueAsNumber);
-          }}
-        />
       </div>
       <div className="flex-grow p-8">
-        <div className="h-full @container relative">
-          <div
-            className="absolute bg-teal-300 rounded-full translate-y-[50%] left-1/2 w-4 h-4"
-            style={{
-              bottom: `${(status.state.y_m / 8000) * 100}%`,
-            }}
-          ></div>
+        <div className="h-full relative" style={{ containerType: "size" }}>
           <div className="absolute bottom-0 flex items-center w-full gap-4 translate-y-[50%]">
             <p className="w-16">0 m</p>
             <hr className="flex-grow border-gray-400" />
@@ -172,11 +209,6 @@ export default function Home() {
           <div className="absolute bottom-1/4 flex items-center w-full gap-4 translate-y-[50%]">
             <p className="w-16">2000 m</p>
             <hr className="flex-grow border-gray-400" />
-          </div>
-          {/* can deploy at 9 s which is 2665 m, coincidentally close to 1/3 of 8000 */}
-          <div className="absolute bottom-1/3 flex items-center w-full gap-4 translate-y-[50%]">
-            <p className="w-16 text-yellow-600">Deploy</p>
-            <hr className="flex-grow border-yellow-600" />
           </div>
           <div className="absolute bottom-[37.5%] flex items-center w-full gap-4 translate-y-[50%]">
             <p className="w-16">3000 m</p>
@@ -194,6 +226,19 @@ export default function Home() {
             <p className="w-16">6000 m</p>
             <hr className="flex-grow border-gray-400" />
           </div>
+          <div className="absolute bottom-[87.5%] flex items-center w-full gap-4 translate-y-[50%]">
+            <p className="w-16">7000 m</p>
+            <hr className="flex-grow border-gray-400" />
+          </div>
+          <div className="absolute bottom-[100%] flex items-center w-full gap-4 translate-y-[50%]">
+            <p className="w-16">8000 m</p>
+            <hr className="flex-grow border-gray-400" />
+          </div>
+          {/* can deploy at 9 s which is 2665 m, coincidentally close to 1/3 of 8000 */}
+          <div className="absolute bottom-1/3 flex items-center w-full gap-4 translate-y-[50%]">
+            <p className="w-16 text-yellow-600">Deploy</p>
+            <hr className="flex-grow border-yellow-600" />
+          </div>
           <div
             className="absolute flex items-center w-full gap-4 translate-y-[50%]"
             style={{
@@ -203,14 +248,44 @@ export default function Home() {
             <p className="w-16 text-purple-600">Target</p>
             <hr className="flex-grow border-purple-600" />
           </div>
-          <div className="absolute bottom-[87.5%] flex items-center w-full gap-4 translate-y-[50%]">
-            <p className="w-16">7000 m</p>
-            <hr className="flex-grow border-gray-400" />
-          </div>
-          <div className="absolute bottom-[100%] flex items-center w-full gap-4 translate-y-[50%]">
-            <p className="w-16">8000 m</p>
-            <hr className="flex-grow border-gray-400" />
-          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 4000 8000"
+            className="h-full absolute left-24"
+          >
+            <path
+              d={
+                "M0 8000" +
+                status.trajectory
+                  .map((state) => `L${state.x_m} ${8000 - state.y_m}`)
+                  .join("")
+              }
+              stroke="#000"
+              fill="none"
+              strokeWidth="1rem"
+              strokeLinecap="round"
+            />
+            <path
+              d={predicted
+                .map(
+                  (state, i) =>
+                    `${i === 0 ? "M" : "L"}${state.x_m} ${8000 - state.y_m}`
+                )
+                .join("")}
+              stroke="#fde047"
+              fill="none"
+              strokeWidth="1rem"
+              strokeLinecap="round"
+            />
+          </svg>
+          <div
+            className="absolute bg-teal-300 rounded-full translate-y-[50%] w-4 h-4 -translate-x-[50%]"
+            style={{
+              // Technically you could use the velocity to interpolate each frame
+              bottom: `${(currentState.y_m / 8000) * 100}%`,
+              left: `calc(6rem + ${(currentState.x_m / 8000) * 100}cqh)`,
+            }}
+          ></div>
         </div>
       </div>
     </main>
