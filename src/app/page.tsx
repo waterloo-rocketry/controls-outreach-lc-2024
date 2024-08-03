2bdb22f54871aa5b040e193de7daddb94c9114c6
Patrick Gu <patrick@patrickgu.ca>
2024-08-03 19:18:13 -0400

    Make the rocket a bit bigger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -291,7 +291,7 @@ export default function Home() {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 6 14"
-              className="w-4 -translate-x-[50%] -translate-y-[50%]"
+              className="w-6 -translate-x-[50%] -translate-y-[50%]"
             >
               <path
                 d={`M 3 0 C 2 1 1 2 1 4 L 1 5 l -${currentState.t_s <= 9 ? 0 : ext / 100} 0 l 0 ${currentState.t_s <= 9 ? 0 : ext / 100} L 1 6 L 1 11 L 0 13 L 0 14 L 2 12 L 2 13 L 4 13 L 4 12 L 6 14 L 6 13 L 5 11 L 5 6 l ${currentState.t_s <= 9 ? 0 : ext / 100} 0 l 0 -${currentState.t_s <= 9 ? 0 : ext / 100} L 5 5 L 5 4 C 5 2 4 1 3 0`}
