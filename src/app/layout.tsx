a2361307348e956ac74098ff128e6166b5c1f52b
Patrick Gu <patrick@patrickgu.ca>
2024-08-11 15:14:44 -0400

    Fix manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Controls Outreach Game",
-  description: "A video game to see what it's like to control a rocket!",
-  manifest: "/manifest.json",
-};
 
 export default function RootLayout({
   children,
